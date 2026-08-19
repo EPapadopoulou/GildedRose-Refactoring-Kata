a828f0a1ebcbd7cc5636c55cad3bb8a3d89e7351
elizapapadopoulou <eliza@code4thought.eu>
2026-08-19 10:42:32 +0300

    Refactor Gilded Rose item update logic by implementing strategy pattern for item quality updates. Introduced specific update strategies for different item types, enhancing maintainability and readability of the code.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,70 @@
 # -*- coding: utf-8 -*-
 
+
+class ItemUpdateStrategy:
+    MAX_QUALITY = 50
+
+    def __init__(self, item):
+        self.item = item
+
+    def update(self):
+        self.update_quality()
+        self.item.sell_in -= 1
+
+    def update_quality(self):
+        raise NotImplementedError
+
+    def increase_quality(self, amount):
+        self.item.quality = min(self.MAX_QUALITY, self.item.quality + amount)
+
+    def decrease_quality(self, amount):
+        self.item.quality = max(0, self.item.quality - amount)
+
+
+class NormalItemUpdateStrategy(ItemUpdateStrategy):
+    def update_quality(self):
+        degradation = 2 if self.item.sell_in <= 0 else 1
+        self.decrease_quality(degradation)
+
+
+class AgedBrieUpdateStrategy(ItemUpdateStrategy):
+    def update_quality(self):
+        increase = 2 if self.item.sell_in <= 0 else 1
+        self.increase_quality(increase)
+
+
+class BackstagePassUpdateStrategy(ItemUpdateStrategy):
+    def update_quality(self):
+        if self.item.sell_in <= 0:
+            self.item.quality = 0
+        elif self.item.sell_in <= 5:
+            self.increase_quality(3)
+        elif self.item.sell_in <= 10:
+            self.increase_quality(2)
+        else:
+            self.increase_quality(1)
+
+
+class SulfurasUpdateStrategy(ItemUpdateStrategy):
+    def update(self):
+        pass
+
+    def update_quality(self):
+        pass
+
+
+STRATEGIES_BY_NAME = {
+    "Aged Brie": AgedBrieUpdateStrategy,
+    "Backstage passes to a TAFKAL80ETC concert": BackstagePassUpdateStrategy,
+    "Sulfuras, Hand of Ragnaros": SulfurasUpdateStrategy,
+}
+
+
+def update_strategy_for(item):
+    strategy = STRATEGIES_BY_NAME.get(item.name, NormalItemUpdateStrategy)
+    return strategy(item)
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,33 +72,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            update_strategy_for(item).update()
 
 
 class Item:
